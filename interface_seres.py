be3c0f2b8bdbb679499dfe28498b82d753217d8e
Samuel Vasconcelos <69234519+samucavm2@users.noreply.github.com>
2024-06-20 15:04:59 -0300

    Update interface_seres.py

--- interface_seres.py
+++ interface_seres.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 class Ser(ABC):
-
+    def __init__(self, nome: str, vida: int = 10, defesa: int = 3) -> None:
+        self.nome: str = nome
+        self.vida: int = vida
+        self.defesa: int = defesa
+        self.iniciativa: int = 0
+        
     @abstractmethod
     def atacar(self, oponente: Optional['Ser']) -> None:
         pass
